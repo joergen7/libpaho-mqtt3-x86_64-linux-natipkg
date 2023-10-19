319b1bda95e51efee99399c37c19c789f87fef5e
Jörgen Brandt <joergen@cuneiform-lang.org>
2023-10-19 11:21:19 +0200

    initial commit

---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang info
+(define collection "libpaho-mqtt3-x86_64-linux-natipkg")
+(define deps '("base"))
+(define build-deps '())
+(define pkg-desc "Provides libpaho-mqtt3 for the package-build server")
+(define version "0.1")
+(define pkg-authors '("Jörgen Brandt"))
+(define license 'Apache-2.0)
+
+(define install-platform "x86_64-linux-natipkg")
+(define copy-foreign-libs '("libpaho-mqtt3c.so.1.3.9"))
